90dae4d24b0fd64d1ef9e7d665f6e9d47670e1ae
masuda <k-masuda@virtualex.local>
2019-05-16 18:03:52 +0900

    create beans

--- src/main/java/beans/Book.java
+++ src/main/java/beans/Book.java
@@ -2,4 +2,54 @@ package beans;
 
 public class Book {
 
+	private int id;
+	private String title;
+	private String genre;
+	private String author;
+	private String publisher;
+	private int purchaseDate;
+	private String buyer;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(int purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public String getBuyer() {
+		return buyer;
+	}
+	public void setBuyer(String buyer) {
+		this.buyer = buyer;
+	}
+
 }
