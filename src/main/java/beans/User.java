f57dad8e2b64c74b6c3093eb047191c1486e3acb
masuda <k-masuda@virtualex.local>
2019-05-16 17:59:36 +0900

    create files

--- src/main/java/beans/User.java
+++ src/main/java/beans/User.java
@@ -1,5 +1,27 @@
 package beans;
 
 public class User {
+	private String mailAdress;
+	private String password;
+	private int manegement;
+	public String getMailAdress() {
+		return mailAdress;
+	}
+	public void setMailAdress(String mailAdress) {
+		this.mailAdress = mailAdress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getManegement() {
+		return manegement;
+	}
+	public void setManegement(int manegement) {
+		this.manegement = manegement;
+	}
+
 
 }
---
+++ src/main/java/dao/Rental_StatusDAO.java
@@ -0,0 +1,5 @@
+package dao;
+
+public class Rental_StatusDAO {
+
+}
