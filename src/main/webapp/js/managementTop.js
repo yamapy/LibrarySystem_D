0981c0d7cb63022c1d68a6cabb52031028c757df
石井 友理 <yu-ishii@virtualex.local>
2019-05-17 21:13:34 +0900

    toptolist

--- src/main/webapp/js/managementTop.js
+++ src/main/webapp/js/managementTop.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+'use strict';
+
+$(function(){
+
+	console.log("Whyyyyy");
+	$('#lendingBookList').click(function() {
+		window.location.href='http://localhost:8080/LibrarySystem_D/html/lendingBookList.html';
+	});
+
+})
