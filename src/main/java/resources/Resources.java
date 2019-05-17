59661ebba99394d4c3790d1f8e9a61fd074ef51d
近藤 しえな <s-kondo@virtualex.local>
2019-05-17 17:06:53 +0900

    0517-3

--- src/main/java/resources/Resources.java
+++ src/main/java/resources/Resources.java
@@ -1,19 +1,11 @@
 package resources;
 
-import java.io.InputStream;
-import java.sql.Date;
-import java.util.List;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+<<<<<<< HEAD
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -28,13 +20,18 @@ import dao.BookDAO;
 
 
 import com.sun.research.ws.wadl.Param;
+=======
+>>>>>>> dev
 
 @Path("resources")
 public class Resources {
 	/**
 	 * 従業員関連のサービス実装。 Servlet/JSPの実装とは異なり、画像についてはバイナリでなくpathベースで扱うものとする。
 	 */
+	@Path("resources")
+	public class EmployeeResource {
 
+<<<<<<< HEAD
 	private final BookDAO bookDAO = new BookDAO();
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -60,37 +57,12 @@ public class Resources {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Employee findById(@PathParam("id") int id) {
 		return empDao.findById(id);
+=======
+>>>>>>> dev
 	}
 
-	/**
-	 * クエリパラメータ指定による検索を実施する。 何も指定しない場合は全件検索になる。
-	 *
-	 * @param postId
-	 *            部署ID。指定しない場合は0が入る。
-	 * @param empId
-	 *            ログイン用の従業員ID。指定しない場合はnullが入る。
-	 * @param nameParam
-	 *            名前の一部を指定するためのパラメータ。指定しない場合はnullが入る。
-	 * @return 取得した従業員情報のリストをJSON形式で返す。データが存在しない場合は空のオブジェクトが返る。
-	 */
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Employee> findByParam(@QueryParam("postId") int postId, @QueryParam("empId") String empId,
-			@QueryParam("nameParam") String nameParam) {
-		Param param = new Param(postId, empId, nameParam);
-		return empDao.findByParam(param);
-	}
-
-	/**
-	 * 指定した従業員情報を登録する。
-	 *
-	 * @param form
-	 *            従業員情報（画像含む）を収めたオブジェクト
-	 * @return DB上のIDが振られた従業員情報
-	 * @throws WebApplicationException
-	 *             入力データチェックに失敗した場合に送出される。
-	 */
 	@POST
+<<<<<<< HEAD
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Employee create(final FormDataMultiPart form) throws WebApplicationException {
@@ -175,114 +147,21 @@ public class Resources {
 		String retireDateStr = form.getField("retireDate").getValue();
 		if (retireDateStr != null && !retireDateStr.isEmpty()) {
 			employee.setRetireDate(retireDateStr);
+=======
+	@Path("{login}")
+	@Consumes("application/x-www-form-urlencoded")
+	// @Produces(MediaType.APPLICATION_JSON)
+	public boolean Login(@FormParam("id") String id, @FormParam("pass") String pass) throws WebApplicationException {
+		String successId = "test";
+		String successPass = "1234";
+		System.out.println(id + pass);
+		if (id != null && pass != null && id.equals(successId) && pass.equals(successPass)) {
+			return true;
+		} else {
+			return false;
+>>>>>>> dev
 		}
 
-		if (!employee.isValidObject()) {
-			throw new WebApplicationException(Response.Status.BAD_REQUEST);
-		}
-
-		// Photo関連の処理
-		String photoIdSrc = form.getField("photoId").getValue();
-		int photoId = Integer.parseInt(photoIdSrc);
-		FormDataBodyPart photoPart = form.getField("photo");
-		if (!photoPart.getContentDisposition().getFileName().isEmpty()) {
-			updatePhoto(photoId, photoPart);
-		}
-		employee.setPhotoId(photoId);
-
-		// Post関連の処理
-		int postId = Integer.parseInt(form.getField("postId").getValue());
-		Post post = postDao.findById(postId);
-		if (post == null) {
-			throw new WebApplicationException(Response.Status.BAD_REQUEST);
-		}
-		employee.setPost(post);
-
-		return empDao.update(employee);
-	}
-
-	/**
-	 * 指定したIDの社員情報を削除する。同時に画像データも削除する。
-	 *
-	 * @param id
-	 *            削除対象の社員情報のID
-	 */
-	@DELETE
-	@Path("{id}")
-	public void remove(@PathParam("id") int id) {
-		Employee employee = empDao.findById(id);
-		empDao.remove(id);
-		photoDao.remove(employee.getPhotoId());
-	}
-
-	@GET
-	@Path("csv")
-	@Produces(MediaType.APPLICATION_OCTET_STREAM)
-	public Response downloadCsv() {
-		Param param = new Param(0, "", "");
-		List<Employee> list = empDao.findByParam(param);
-
-		String header = "ID,社員番号,名前,年齢,性別,写真ID,郵便番号,都道府県,住所,所属部署ID,入社日付,退社日付" + System.getProperty("line.separator");
-		StringBuffer csvContents = new StringBuffer(header);
-
-		for (Employee employee : list) {
-			String line = employee.toString() + System.getProperty("line.separator");
-			csvContents.append(line);
-		}
-
-		return Response.status(Status.OK).entity(csvContents.toString())
-				.header("Content-disposition", "attachment; filename=employee.csv").build();
-	}
-
-	/**
-	 * Formから渡されたデータを使用してPhotoデータを登録する。
-	 *
-	 * @param photoPart
-	 *            Formから渡されたPhotoデータ
-	 * @return 登録されてIDが振られたPhotoインスタンス
-	 */
-	private Photo createPhoto(FormDataBodyPart photoPart) {
-		Photo photo = build(photoPart);
-
-		return photoDao.create(photo);
-	}
-
-	/**
-	 * Formから渡されたデータを使用してPhotoデータを更新する。
-	 *
-	 * @param photoId
-	 *            更新対象のPhotoのID
-	 * @param photoPart
-	 *            Formから渡されたPhotoデータ
-	 * @return 正常に更新された場合はtrue、失敗した場合はfalse
-	 */
-	private boolean updatePhoto(int photoId, FormDataBodyPart photoPart) {
-		Photo photo = build(photoPart);
-		photo.setId(photoId);
-		return photoDao.update(photo);
-	}
-
-	/**
-	 * formから渡されたデータを使用してPhotoインスタンスを構築する。
-	 *
-	 * @param photoPart
-	 *            Formから渡されたPhotoデータ
-	 * @return ID以外のフィールドに値がセットされたPhotoインスタンス
-	 */
-	private Photo build(FormDataBodyPart photoPart) {
-		Photo photo = new Photo();
-		ContentDisposition photoInfo = photoPart.getContentDisposition();
-
-		photo.setFileName(photoInfo.getFileName());
-
-		photo.setContentType(photoPart.getMediaType().toString());
-
-		BodyPartEntity bodyPartEntity = (BodyPartEntity) photoPart.getEntity();
-		InputStream in = bodyPartEntity.getInputStream();
-		photo.setPhoto(in);
-
-		photo.setEntryDate(new Date(System.currentTimeMillis()));
-		return photo;
 	}
 
 }
--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,3 +1,35 @@
-/**
- * 
- */
+function login() {
+	// 入力されたユーザーIDとパスワード
+	var requestQuery = {
+		id : $('#login-id').val(),
+		pass : $('#login-pass').val()
+	};
+	console.log($('#login-id').val());
+	$.ajax({
+		url : "/LibrarySystem_D/api/v1.1/resources/login",
+		type : "POST",
+		data : requestQuery,
+		dataType : "json",
+		success : function(data) {
+			console.log(data);
+			if (data == true) {
+				alert('ログイン成功');
+//				location.href = './Expense.html';
+			} else {
+				alert('ログイン失敗');
+			}
+
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('通信失敗');
+		}
+	})
+
+}
+
+$(document).ready(function() {
+
+	// ログインボタンを押したときのイベント
+	$('#login-button').click(login);
+
+});
