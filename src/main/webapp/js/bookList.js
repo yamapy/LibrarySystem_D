6770470370f4d00056e103c525ffe4d8e9ec7c4a
近藤 しえな <s-kondo@virtualex.local>
2019-05-21 18:25:57 +0900

    pullして

--- src/main/webapp/js/bookList.js
+++ src/main/webapp/js/bookList.js
@@ -1,3 +1,5 @@
+
+
 'use strict';
 
 var rootUrl = "/LibrarySystem_D/api/v1.1/resources";
@@ -43,50 +45,46 @@ function renderTable(data) {
 
 		var headerRow = '<tr><th>タイトル</th><th>ジャンル</th><th>作者</th><th>詳細</th><th>貸出</th>';
 
-				$('#book').children().remove();
+		$('#book').children().remove();
 
 
 
-				if (data.length === 0) {
-					$('#book').append('<p>現在データが存在していません。</p>')
-				} else {
-					var table = $('<table>').attr('border', 1);
-					table.append(headerRow);
+		if (data.length === 0) {
+			$('#book').append('<p>現在データが存在していません。</p>')
+		} else {
+			var table = $('<table>').attr('border', 1);
+			table.append(headerRow);
 
 
-					$.each(data, function(index, book) {
-						var row = $('<tr>');
-						row.append($('<td>').text(book.title).attr("id","title"));
-						row.append($('<td nowrap>').text(book.genre).attr("id","genre"));
-						row.append($('<td>').text(book.author).attr("id","author"));
-						row.append($('<td nowrap>').append(
-								$('<button>').text("詳細").attr("type","button").attr("id","bookDetail")
-						));
+			$.each(data, function(index, book) {
+				var row = $('<tr>');
+				row.append($('<td>').text(book.title).attr("id","title"));
+				row.append($('<td nowrap>').text(book.genre).attr("id","genre"));
+				row.append($('<td>').text(book.author).attr("id","author"));
+				row.append($('<td nowrap>').append(
+						$('<button>').text("詳細").attr("type","button").attr("id","bookDetail")
+				));
 
-						if(book.status=="貸出中"){
-						row.append($('<td nowrap>').text(book.status).attr("id","status").attr("class","red"));
-						}else{
-							row.append($('<td nowrap>').append(
-									$('<button>').text("借りる").attr("type","button").attr("id",book.id).attr("class","borrow")
-							));
-						}
-
-						table.append(row);
-					});
-
-					$('#book').append(table);
-					$('.borrow').click(borrowById);
+				if(book.status=="貸出中"){
+					row.append($('<td nowrap>').text(book.status).attr("id","status").attr("class","red"));
+				}else{
+					row.append($('<td nowrap>').append(
+							$('<button>').text("借りる").attr("type","button").attr("id",book.id).attr("class","borrow")
+					));
 				}
+
+				table.append(row);
+			});
+
+			$('#book').append(table);
+			$('.borrow').click(borrowById);
+		}
 
 
 
 	}
 
 	else{
-
-		//loginButton();
-
-
 		var headerRow = '<tr><th>タイトル</th><th>ジャンル</th><th>作者</th><th>詳細</th><th>ステータス</th>';
 
 		$('#book').children().remove();
@@ -109,7 +107,6 @@ function renderTable(data) {
 						$('<button>').text("詳細").attr("type","button").attr("id","bookDetail").attr("class","grayButton")
 				));
 
-//				row.append($('<td>').text(book.status));
 				if(book.status=="貸出中"){
 					row.append($('<td nowrap>').text(book.status).attr("id","status").attr("class","red"));
 				}else
@@ -137,9 +134,7 @@ $('#findBook').click(function() {
 	return false;
 })
 
-//$('.borrow').click(function() {
-//	borrowById();
-//})
+
 
 function initPageGenre() {
 	var newOption = $('<option>').val("").text('指定しない').prop('selected', true);
@@ -153,9 +148,9 @@ function findByParam() {
 	console.log('findByParam start.');
 
 	var urlWithParam = rootUrl+"/findByParam"+'?titleParam='+$('#titleParam').val()
-		+'&authorParam='+$('#authorParam').val()
-		+'&genre='+$('#genreParam').val()
-		+'&status='+$('#statusParam').val();
+	+'&authorParam='+$('#authorParam').val()
+	+'&genre='+$('#genreParam').val()
+	+'&status='+$('#statusParam').val();
 	console.log( urlWithParam);
 	$.ajax({
 		type : "GET",
@@ -183,10 +178,9 @@ function makeGenreSelection(selectionGenre, book) {
 }
 
 function borrowById(button) {
-var id = button.currentTarget.id;
-var requestQuery = {id:id};
+	var id = button.currentTarget.id;
+	var requestQuery = {id:id};
 	console.log('borrow start - id:' + id);
-//	var fd = {id:id};
 	$.ajax({
 		type : "POST",
 		url : rootUrl + '?id='+id,
